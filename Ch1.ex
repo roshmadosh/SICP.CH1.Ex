02e24f4e0de53986f12b468cb97fa4a38ea33200
roshmadosh <hiroshi.nobuoka@gmail.com>
2020-12-15 12:00:47 -0500

    1.6

---
+++ Ch1.ex
@@ -0,0 +1,11 @@
+#1.6
+(define (average a b) (/ (+ a b) 2))
+(define (square s) (* s s))
+(define (abs v) (if (< v 0) (- v) v))
+(define (goodenough g x) (< (abs (- (square g) x)) .001))
+(define (improve guess x) (average guess (/ x guess)))
+
+(define (sqrt-iter guess x)
+  (if (goodenough guess x) guess
+  (sqrt-iter (improve guess x) x)))
+(define (sqrt r) (sqrt-iter 1.0 r))
